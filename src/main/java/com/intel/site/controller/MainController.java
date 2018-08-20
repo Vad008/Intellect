b2b144081ed28afb43beff940f05bd717c01a43f
Vadim <Vad008@ukr.net>
2018-08-20 17:00:08 +0300

    work version

--- src/main/java/com/intel/site/controller/MainController.java
+++ src/main/java/com/intel/site/controller/MainController.java
@@ -1,5 +1,7 @@
 package com.intel.site.controller;
 
+import com.intel.site.dto.Book;
+import com.intel.site.dto.Record;
 import com.intel.site.dto.Sertificate;
 import com.intel.site.services.DbService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
 
 @Controller
@@ -65,24 +70,91 @@ public class MainController {
     }
 
     @RequestMapping(value = "admin", method = RequestMethod.POST)
-    public String admen(@RequestParam int id) {
-        dbService.changeDone(id, true);
-        return "admin";
-    }
-
-    @RequestMapping(value = "adminpage", method = RequestMethod.POST)
-    public String adminPage(@RequestParam String admin, Model model) {
-        if (admin.equals("vad")){
-            List<Sertificate> list = dbService.getAllSertificates();
-            model.addAttribute("list",list);
-            System.out.println(list.get(0).getName());
-            return "adminpage";
+    public String admen(@RequestParam(required = false) Integer id, @RequestParam(required = false) Integer idquest) {
+        if (id != null) {
+            dbService.changeDone(id, true);
+        }
+        if (idquest != null) {
+            dbService.changeDonebook(idquest, true);
         }
         return "admin";
     }
 
-    @RequestMapping(value = "booking")
-    public String booking(){
+    @RequestMapping(value = "adminmenu", method = RequestMethod.POST)
+    public String adminmenu(@RequestParam String admin, HttpServletRequest req) {
+        if (admin.equals("vad")) {
+            req.getSession().setAttribute("admin", admin);
+            return "adminmenu";
+        }
+        return "admin";
+    }
+
+
+    @RequestMapping(value = "adminpage", method = RequestMethod.POST)
+    public String adminPage(Model model, HttpServletRequest req, @RequestParam String action) {
+        if (req.getSession().getAttribute("admin").equals("vad")) {
+            switch (action) {
+                case "sert":
+                    List<Sertificate> list = dbService.getAllSertificates();
+                    model.addAttribute("list", list);
+                    System.out.println(list.get(0).getName());
+                    return "adminpage";
+                case "book":
+                    model.addAttribute("books", dbService.getAllBooks());
+                    return "adminbooking";
+                case "record":
+                    List<Record> kush = dbService.getKushRecords(dbService.getAllRecords());
+                    List<Record> clinic = dbService.getClinicRecords(dbService.getAllRecords());
+                    Collections.sort(kush);
+                    Collections.sort(clinic);
+                    model.addAttribute("kush", kush);
+                    model.addAttribute("clinic", clinic);
+                    return "recordsadmin";
+                default:
+                    return "adminmenu";
+
+            }
+        }
+        return "admin";
+    }
+
+    @RequestMapping(value = "booking", method = RequestMethod.GET)
+    public String booking() {
         return "booking";
     }
+
+    @RequestMapping(value = "booking", method = RequestMethod.POST)
+    public String endBooking(Model model, @RequestParam String name, @RequestParam String phone,
+                             @RequestParam String email, @RequestParam String time, @RequestParam String message) {
+        dbService.addBook(new Book(dbService.getNewIdBook(), name, phone, email, time, "Куш", message));
+        model.addAttribute("isGood", "Квет забронирован успешно");
+        return "booking";
+    }
+
+    @RequestMapping(value = "bookingclin", method = RequestMethod.GET)
+    public String bookingClin() {
+        LocalDateTime lcdt = LocalDateTime.now();
+        System.out.println(lcdt);
+        return "bookingclin";
+    }
+
+    @RequestMapping(value = "bookingclin", method = RequestMethod.POST)
+    public String endBookingClin(Model model, @RequestParam String name, @RequestParam String time) {
+        System.out.println(time);
+        model.addAttribute("isGood", "Квет забронирован успешно");
+        return "bookingclin";
+    }
+
+    @RequestMapping(value = "records")
+    public String records(Model model) {
+        List<Record> kush = dbService.getKushRecords(dbService.getAllRecords());
+        List<Record> clinic = dbService.getClinicRecords(dbService.getAllRecords());
+        Collections.sort(kush);
+        Collections.sort(clinic);
+        model.addAttribute("kush", kush);
+        model.addAttribute("clinic", clinic);
+        return "records";
+    }
+
+
 }
--- src/main/java/com/intel/site/services/DbService.java
+++ src/main/java/com/intel/site/services/DbService.java
@@ -1,10 +1,13 @@
 package com.intel.site.services;
 
 
+import com.intel.site.dto.Book;
+import com.intel.site.dto.Record;
 import com.intel.site.dto.Sertificate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -46,11 +49,80 @@ public class DbService {
                 , param);
     }
 
-    public void changeDone(int id, boolean done){
+    public void changeDone(int id, boolean done) {
         Map<String, Object> param = new HashMap<>();
         param.put("id", id);
-        param.put("done",done);
-        jdbcTemplate.update("UPDATE sertificates SET done = :done WHERE id = :id",param);
+        param.put("done", done);
+        jdbcTemplate.update("UPDATE sertificates SET done = :done WHERE id = :id", param);
+    }
+
+    public List<Book> getAllBooks() {
+        return jdbcTemplate.query("SELECT id,name,phonenumber,email,datequest,quest, messageuser,done FROM booking",
+                (rs, rowNum) -> new Book(rs.getInt("id"),
+                        rs.getString("name"), rs.getString("phonenumber"),
+                        rs.getString("email"), rs.getString("datequest"),rs.getString("quest"),
+                        rs.getString("messageuser"), rs.getBoolean("done")));
+    }
+
+    public int getNewIdBook() {
+        int i = 0;
+        for (Book sert : getAllBooks()) {
+            if (i <= sert.getId()) {
+                i = sert.getId() + 1;
+            }
+        }
+        return i;
+    }
+
+    public void addBook(Book book) {
+        Map<String, Object> param = new HashMap<>();
+        param.put("id", book.getId());
+        param.put("name", book.getName());
+        param.put("phone", book.getPhone());
+        param.put("email", book.getEmail());
+        param.put("quest", book.getQuest());
+        param.put("datequest", book.getDatequest());
+        param.put("messageuser", book.getMessageuser());
+        param.put("done", book.isDone());
+        jdbcTemplate.update("INSERT INTO booking(id,name,phonenumber,email,datequest,quest,messageuser,done) " +
+                        "VALUES (:id,:name,:phone,:email,:datequest,:quest,:messageuser,:done)"
+                , param);
+    }
+
+    public void changeDonebook(int id, boolean done) {
+        Map<String, Object> param = new HashMap<>();
+        param.put("id", id);
+        param.put("done", done);
+        jdbcTemplate.update("UPDATE booking SET done = :done WHERE id = :id", param);
+    }
+
+    public List<Record> getAllRecords(){
+        return jdbcTemplate.query("SELECT id,crew,timeexit,quest FROM records",
+                (rs, rowNum) -> new Record(rs.getInt("id"),
+                        rs.getString("crew"), rs.getFloat("timeexit"),
+                        rs.getString("quest")));
+    }
+
+
+    public List<Record> getClinicRecords(List<Record> records){
+        List<Record> list = new ArrayList<>();
+        for (Record record: records){
+            if (record.getQuest().equals("Клініка")){
+                list.add(record);
+            }
+        }
+        return list;
+    }
+
+
+    public List<Record> getKushRecords(List<Record> records){
+        List<Record> list = new ArrayList<>();
+        for (Record record: records){
+            if (record.getQuest().equals("Куш")){
+                list.add(record);
+            }
+        }
+        return list;
     }
 
 
